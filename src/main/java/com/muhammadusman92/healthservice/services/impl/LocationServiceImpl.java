900eedd9a09f7d0149032e7d905202ed3bd4a8e5
MuhammadUsman92 <usmankhokhar0222@gmail.com>
2023-07-10 17:36:30 +0000

    Final Commit

--- src/main/java/com/muhammadusman92/healthservice/services/impl/LocationServiceImpl.java
+++ src/main/java/com/muhammadusman92/healthservice/services/impl/LocationServiceImpl.java
@@ -20,27 +20,38 @@ public class LocationServiceImpl implements LocationService {
     private LocationRepo locationRepo;
     @Autowired
     private ConversionDtos conversionDtos;
+
     @Override
     public List<PatientDto> getAllPatientsWithInLocation(double latitude, double longitude, double radius) {
         double radiusInDegrees = radius / 111120.0;
-
         double minLatitude = latitude - radiusInDegrees;
         double maxLatitude = latitude + radiusInDegrees;
-
         double degreesPerLongitude = Math.cos(Math.toRadians(latitude)) * 111120.0;
         double radiusLongitude = radius / degreesPerLongitude;
-
         double minLongitude = longitude - radiusLongitude;
         double maxLongitude = longitude + radiusLongitude;
         List<Patient> patients = locationRepo.findAllPatientsWithinLocation(minLatitude, maxLatitude, minLongitude, maxLongitude);
+        List<PatientDto> patientDtoList = new ArrayList<>();
+        for (Patient patient : patients) {
+            double patientLatitude = patient.getLocation().getLatitude();
+            double patientLongitude = patient.getLocation().getLongitude();
 
-        List<PatientDto> patientDtoList =new ArrayList<>();
-        for(Patient patient:patients){
-            PatientDto patientDto = conversionDtos.patientToPatientDto(patient);
-            patientDto.setDiseaseSet(patient.getDiseaseSet().stream().map(disease->conversionDtos.diseaseToDiseaseDto(disease)).collect(Collectors.toSet()));
-//            patientDto.setLocation(conversionDtos.locationToLocationDto(patient.getLocation()));
-            patientDtoList.add(patientDto);
+            double distance = calculateDistance(latitude, longitude, patientLatitude, patientLongitude, radiusInDegrees);
+            if (distance <= radiusInDegrees) {
+                PatientDto patientDto = conversionDtos.patientToPatientDto(patient);
+                patientDto.setDiseaseSet(patient.getDiseaseSet().stream().map(disease -> conversionDtos.diseaseToDiseaseDto(disease)).collect(Collectors.toSet()));
+                patientDtoList.add(patientDto);
+            }
         }
         return patientDtoList;
     }
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2, double radius) {
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = radius * c;
+        return distance;
+    }
+
 }
